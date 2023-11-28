85625f7076126c6002507f2a79c562b7753bc0a0
Camilo Yate <89857691+AnaliticaRacafe@users.noreply.github.com>
2023-11-28 09:39:04 -0500

    Update Funciones.r

--- Funciones.r
+++ Funciones.r
@@ -760,7 +760,7 @@ CajaIco <- function(texto, icono, col_fondo, col_letra = "#17202A", col_icono = 
   require(colorspace)
   
   # Definición de estilos para la caja
-  s_caj <- paste0("display:block;background:", col_fondo, ";min-height:120px;width:100%;border-radius:10px;box-shadow:1px 1px 2px", darken(col_fondo, 0.1), ";")
+  s_caj <- paste0("display:block;background:", col_fondo, ";min-height:120px;height: fit-content;width:100%;border-radius:10px;box-shadow:1px 1px 2px", darken(col_fondo, 0.1), ";")
   
   # Definición de estilos para el ícono
   s_ico <- paste0("position:absolute;display:block;float:left;height:100%;width:100%;text-align:left; font-size:80px;color:", adjust_transparency(col_icono, 0.05), ";background:transparent;z-index:1")
